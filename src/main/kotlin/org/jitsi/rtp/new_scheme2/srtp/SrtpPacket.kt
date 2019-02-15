6b310ad1e1ff459667a0208347b4f9f816082a1b
Brian Baldino <bbaldino@gmail.com>
2019-02-15 14:33:39 -0800

    immutable srtp packet

---
+++ src/main/kotlin/org/jitsi/rtp/new_scheme2/srtp/SrtpPacket.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.rtp.new_scheme2.srtp
+
+import org.jitsi.rtp.extensions.subBuffer
+import org.jitsi.rtp.new_scheme2.rtp.ImmutableRtpHeader
+import org.jitsi.rtp.new_scheme2.rtp.ImmutableRtpPacket
+import org.jitsi.rtp.util.ByteBufferUtils
+import java.nio.ByteBuffer
+
+/**
+ * An SRTP packet
+ * TODO(brian): i wish we could know the auth tag length
+ * when creating this, but we need some form of SRTP packet
+ * (distinct from SRTCP) before we know it.  we could do something
+ * like 'ImmutableSrtpPacketWithUnknownTag' (or something less
+ * ridiculous) and only transition to a type which parsed the
+ * tag once we know its length?
+ */
+class ImmutableSrtpPacket(
+    header: ImmutableRtpHeader = ImmutableRtpHeader(),
+    payload: ByteBuffer = ByteBufferUtils.EMPTY_BUFFER,
+    backingBuffer: ByteBuffer? = null
+) : ImmutableRtpPacket(header, payload, backingBuffer) {
+
+    fun getAuthTag(tagLength: Int) =
+            dataBuf.subBuffer(dataBuf.limit() - tagLength)
+
+}
+
---
+++ src/main/kotlin/org/jitsi/rtp/new_scheme2/srtp/SrtpProtocolPacket.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.rtp.new_scheme2.srtp
+
+import org.jitsi.rtp.new_scheme2.ImmutablePacket
+import java.nio.ByteBuffer
+
+
+/**
+ * [UnparsedReadOnlySrtpPacket] is either an SRTP packet or SRTCP packet (but we don't know which)
+ * so it basically just distinguishes a packet as encrypted and stores the buffer
+ */
+open class ImmutableUnparsedSrtpProtocolPacket(
+    override val dataBuf: ByteBuffer
+) : ImmutablePacket()
+
+fun main() {
+    val isp = ImmutableSrtpPacket()
+}
