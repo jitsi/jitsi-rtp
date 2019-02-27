62a7dfdd3ab398d7d73219fa22e23a44692c189d
Brian Baldino <bbaldino@gmail.com>
2019-02-26 16:39:52 -0800

    re-add pli packet

---
+++ src/main/kotlin/org/jitsi/rtp/new_scheme3/rtcp/rtcpfb/RtcpFbPliPacket.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.rtp.new_scheme3.rtcp.rtcpfb
+
+import org.jitsi.rtp.extensions.subBuffer
+import org.jitsi.rtp.new_scheme3.rtcp.RtcpHeader
+import org.jitsi.rtp.new_scheme3.rtcp.rtcpfb.fci.Pli
+import java.nio.ByteBuffer
+
+/**
+ * https://tools.ietf.org/html/rfc4585#section-6.3.1
+ * PLI does not require parameters.  Therefore, the length field MUST be
+ *  2, and there MUST NOT be any Feedback Control Information.
+ */
+class RtcpFbPliPacket(
+    header: RtcpHeader = RtcpHeader(),
+    mediaSourceSsrc: Long = -1,
+    backingBuffer: ByteBuffer? = null
+) : PayloadSpecificFbPacket(header.modify { reportCount = FMT }, mediaSourceSsrc, Pli(), backingBuffer) {
+
+    override fun clone(): org.jitsi.rtp.new_scheme3.Packet {
+        return RtcpFbPliPacket(cloneMutableHeader(), mediaSourceSsrc)
+    }
+
+    companion object {
+        const val FMT = 1
+
+        fun fromBuffer(buf: ByteBuffer): RtcpFbPliPacket {
+            val bufStartPosition = buf.position()
+            val header = RtcpHeader.create(buf)
+            val mediaSourceSsrc = RtcpFbPacket.getMediaSourceSsrc(buf)
+            return RtcpFbPliPacket(header, mediaSourceSsrc, buf.subBuffer(bufStartPosition, buf.position()))
+        }
+    }
+}
