7713f05ee8596f55f4b08cb0885b7a770d8acb5d
Brian Baldino <bbaldino@gmail.com>
2019-02-26 23:22:34 -0800

    add new helper for reparsing an rtppacket as an rtxpacket

--- src/main/kotlin/org/jitsi/rtp/new_scheme3/rtp/RtxPacket.kt
+++ src/main/kotlin/org/jitsi/rtp/new_scheme3/rtp/RtxPacket.kt
@@ -17,6 +17,7 @@
 package org.jitsi.rtp.new_scheme3.rtp
 
 import org.jitsi.rtp.extensions.subBuffer
+import org.jitsi.rtp.extensions.unsigned.toPositiveInt
 import org.jitsi.rtp.util.ByteBufferUtils
 import java.nio.ByteBuffer
 
@@ -32,14 +33,27 @@ class RtxPacket internal constructor(
 ) : RtpPacket(header, payload, backingBuffer) {
 
     companion object {
+        // Create an RTX packet (to be sent out) from a previously-sent
+        // RTP packet
         fun fromRtpPacket(rtpPacket: RtpPacket): RtxPacket {
             return rtpPacket.toOtherRtpPacketType { rtpHeader, payload, backingBuffer ->
                 RtxPacket(rtpHeader, payload, rtpPacket.header.sequenceNumber, backingBuffer)
-            } as RtxPacket
+            }
         }
+        // Convert a packet, currently parsed as an RTP packet, to an RTX packet
+        // (i.e., an incoming packet, previously parsed as an RTP packet, has been
+        // determined to actually be an RTX packet, so re-parse it as such
+        fun parseAsRtx(rtpPacket: RtpPacket): RtxPacket {
+            return rtpPacket.toOtherRtpPacketType { rtpHeader, payload, backingBuffer ->
+                val originalSequenceNumber = payload.getShort().toPositiveInt()
+                val actualPayload = payload.subBuffer(payload.position())
+                RtxPacket(rtpHeader, actualPayload, originalSequenceNumber, backingBuffer )
+            }
+        }
+        // Parse a buffer as an RTX packet
         fun fromBuffer(buf: ByteBuffer): RtxPacket {
             val header = RtpHeader.create(buf)
-            val originalSequenceNumber = buf.getShort(header.sizeBytes.toInt()).toInt()
+            val originalSequenceNumber = buf.getShort(header.sizeBytes).toInt()
             val payload = buf.subBuffer(header.sizeBytes + 2)
 
             return RtxPacket(header, payload, originalSequenceNumber, buf)
