3aeaa82b87a078295c06784451f7fbff7d3a85cc
George Politis <gp@jitsi.org>
2019-12-09 16:26:27 +0000

    adds a test of parsing a verbatim REMB packet

--- src/test/kotlin/org/jitsi/rtp/rtcp/rtcpfb/payload_specific_fb/RtcpFbRembPacketTest.kt
+++ src/test/kotlin/org/jitsi/rtp/rtcp/rtcpfb/payload_specific_fb/RtcpFbRembPacketTest.kt
@@ -19,9 +19,29 @@ import io.kotlintest.IsolationMode
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.ShouldSpec
 import org.jitsi.rtp.rtcp.RtcpHeaderBuilder
+import org.jitsi.rtp.util.byteBufferOf
 
 internal class RtcpFbRembPacketTest : ShouldSpec() {
     override fun isolationMode(): IsolationMode? = IsolationMode.InstancePerLeaf
+
+    val rembPacket1Buf = byteBufferOf(
+            0x8f, 0xce, 0x00, 0x05,
+            0xfe, 0x93, 0x23, 0x03,
+            0x00, 0x00, 0x00, 0x00,
+            0x52, 0x45, 0x4d, 0x42,
+            0x01, 0x0b, 0xa4, 0x5a,
+            0x9e, 0x72, 0x01, 0xc1
+    )
+
+    val rembPacket2Buf = byteBufferOf(
+            0x8f, 0xce, 0x00, 0x06,
+            0xfe, 0x93, 0x23, 0x03,
+            0x00, 0x00, 0x00, 0x00,
+            0x52, 0x45, 0x4d, 0x42,
+            0x02, 0x0b, 0xb4, 0x5a,
+            0x9e, 0x72, 0x01, 0xc1,
+            0x9e, 0x23, 0x54, 0xa3
+    )
 
     init {
         "Creating an RtcpFbRembPacket" {
@@ -38,6 +58,28 @@ internal class RtcpFbRembPacketTest : ShouldSpec() {
                     RtcpFbRembPacket.getBrMantissa(rembPacket.buffer, 0) shouldBe 250000
                     rembPacket.ssrcs shouldBe listOf(1234L)
                     rembPacket.bitrate shouldBe 1_000_000L
+                }
+            }
+            "creation" {
+                "from a buffer" {
+                    val rembPacket = RtcpFbRembPacket(rembPacket1Buf.array(), rembPacket1Buf.arrayOffset(), rembPacket1Buf.limit())
+                    should("read everything correctly") {
+                        rembPacket.senderSsrc shouldBe 0xfe932303
+                        rembPacket.numSsrc shouldBe 1
+                        rembPacket.ssrcs shouldBe listOf(0x9e7201c1)
+                        rembPacket.bitrate shouldBe 954728
+                    }
+                }
+            }
+            "creation" {
+                "from a buffer" {
+                    val rembPacket = RtcpFbRembPacket(rembPacket2Buf.array(), rembPacket2Buf.arrayOffset(), rembPacket2Buf.limit())
+                    should("read everything correctly") {
+                        rembPacket.senderSsrc shouldBe 0xfe932303
+                        rembPacket.numSsrc shouldBe 2
+                        rembPacket.ssrcs shouldBe listOf(0x9e7201c1, 0x9e2354a3)
+                        rembPacket.bitrate shouldBe 971112
+                    }
                 }
             }
         }
